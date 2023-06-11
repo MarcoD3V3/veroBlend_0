0d197624952ae19ab926760504e201eb15d1d44b
MarcoD3V3 <git.sinister.developer@gmail.com>
2023-06-11 12:43:47 -0500

    se creo los api/drinks

---
+++ src/app/Resources/Api/Drinks/Drinks.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './Drinks.module.scss';
+
+export default function Drinks() {
+	return <div className={styles.Drinks}>Drinks</div>;
+}
---
+++ src/app/Resources/Api/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './Api.module.scss';
+
+export default function Api() {
+	return (
+		<div className={styles.api}>
+			Api
+		</div>);
+}
