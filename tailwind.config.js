a513d45b54c9430ed70fb03bad52d6d339f17f3e
MarcoD3V3 <124552860+MarcoD3V3@users.noreply.github.com>
2023-06-19 00:46:24 +0000

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,14 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
+		extend: {
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
+			},
+		},
+	},
+	plugins: [require('tw-elements/dist/plugin.cjs')],
+	darkMode: 'class',
+};
