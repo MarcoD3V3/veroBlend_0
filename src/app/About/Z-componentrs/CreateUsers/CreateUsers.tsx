42ef06c8a690ac9755aa8eb10aacf789b7ed075f
marcoD3V3 <git.sinister.developer@gmail.com>
2023-06-23 20:56:41 -0500

    agregando componentes para crear usuarios (Prueva)

--- src/app/About/Z-componentrs/CreateUsers/CreateUsers.tsx
+++ src/app/About/Z-componentrs/CreateUsers/CreateUsers.tsx
@@ -22,19 +22,23 @@ const CreateUsers: React.FC<CreateUsersProps> = ({id, userName, imageUrl}) => {
 
 	function handler(): void {
 		if (urlRef.current && nameRef.current) {
-			console.log(urlRef.current.value);
-			setNameUser(nameRef.current.value);
-			setUrlImage(urlRef.current.value);
-		} else {
-			setError('rellenar todos los campos: ');
+			const NameValue = nameRef.current.value;
+			const UrlValue = urlRef.current.value;
+			if (UrlValue) {
+				console.log(urlRef.current.value);
+				setNameUser(nameRef.current.value);
+				setUrlImage(urlRef.current.value);
+			}	else {
+				setError('rellenar todos los campos: ');
+			}
 		}
 	}
 
 	return (
 		<div className={styles.createUsers}>
 			<form>
-				<input type="text" placeholder="name" className={styles.NAMEUSER} ref={nameRef} />
-				<input type="text" placeholder="imageURL" className={styles.URLIMAGE} ref={urlRef} />
+				<input type="text" placeholder="name" className='bg-black' ref={nameRef} />
+				<input type="text" placeholder="imageURL" className='bg-black' ref={urlRef} />
 				<input type="button" value="Create" onClick={handler} />
 				<div>{nameUser}</div>
 				<div>{urlImage}</div>
